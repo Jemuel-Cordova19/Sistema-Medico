ff65f4492ed9bc4b932d97fc3f996729e8975415
Jemuel-Cordova19 <cordovajemuel0@gmail.com>
2026-09-08 18:51:09 -0600

    cambiar codigo en main.py para probar las subramas

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,11 @@
-# Servidor Backend Principal
-print("Backend listo para configurarse") 
+from fastapi import FastAPI
+
+# Creamos la instancia de la aplicación
+app = FastAPI(title="Sistema Médico API")
+
+# Ruta principal de prueba
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "Servidor del Sistema Médico funcionando correctamente desde la subrama"
+    }
